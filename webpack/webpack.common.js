a7b489f30bef675120c6eab77102ada00aba9904
Vlad Apetre <vlad.apetre@expertnetwork.ro>
2023-04-24 12:38:25 +0300

    cache busting

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -44,7 +44,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, '..', './build'),
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
   },
   plugins: [
     new HtmlWebpackPlugin({
